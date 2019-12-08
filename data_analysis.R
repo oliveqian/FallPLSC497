8d3a2e40d5990de8727cbe75d1d27231c5bac8bb
oliverqian <oliverqian@live.com>
2019-12-08 14:16:07 -0500

    Some more progress on the plot. Still need to have a control variable.

--- data_analysis.R
+++ data_analysis.R
@@ -15,6 +15,10 @@ setnames(FDI, "year", "Year")
 rail <- read_excel('rail_length.xlsx')
 jour_num <- read_excel('Journal_numbers.xlsx')
 education <- read_excel('education_c.xlsx')
+education_russia <- read_excel('education_c_v.xlsx')
+GDP_R <- read_excel('GDP_R.xlsx')
+FDI_R <- read_excel('FDI_R.xlsx')
+
 
 
 Merged_fdi <- merge(FDI, GDP_C, by="Year")
@@ -37,9 +41,12 @@ p3 <- ggplot(merged_fdi_percent, aes(x = merged_fdi_percent$`as % of GDP`, y = m
   geom_line()+
   geom_point(size = 1, color = "red")
 
-#plot for the realtion between 
+#plot for the realtion between education and GDP
 merge_edu_GDP <- merge(GDP_C, education, by = "Year")
-p5 <- ggplot()
+p5 <- ggplot(merge_edu_GDP, aes(x = merge_edu_GDP$Index, y = merge_edu_GDP$GDP))+
+  geom_smooth(method = "lm")+
+  geom_point(size = 2, color = "red" )+
+  labs(x = "Education index", y = "GDP", title = "Relationship between Education index and GDP in China", caption = 'data from World Bank Group')
 
 
 #drop 2 comuns in education
@@ -80,4 +87,42 @@ stargazer(multi_re,type = 'html',out = 'multi_re.htm',
           covariate.labels = c("FDI","Expenditure", "Journal Numbers","Rail length", "Education index")
 )
 shell.exec('multi_re.htm')
+
+# Include other authoritarian countries- Russia
+
+#education index
+setnames(education, "Index", "Index_c" )
+education_compa <- merge(education, education_russia, by  = "Year")
+education_compa$Year <- as.numeric(as.character(education_compa$Year))
+education_compa$Index_c <- as.numeric(education_compa$Index_c)
+education_compa$Index <- as.numeric(education_compa$Index)
+
+p6 <- ggplot(education_compa, aes(x = Year))+
+  geom_line(aes(y= education_compa$Index_c),size = 1.5, colour= "red", linetype = "solid" )+
+  geom_line(aes(y = education_compa$Index), size = 1.5, colour = "blue", linetype ="solid")+
+  annotate("text", x = 2005, y = 0.4, label = "education index of China", colour = "Red", size = 5)+
+  annotate("text", x = 2005, y = 0.66, label = "education index of Russia", colour = "blue", size = 5)+
+  labs(title = "Comparison between two countries' education index ", caption = "data from World Bank")
+
+#education and GDP 
+merge_edu_GDP_r <- merge(GDP_R, education_russia, by ="Year")
+merge_edu_GDP_r$Year <- as.numeric(merge_edu_GDP_r$Year)
+merge_edu_GDP_r$GDP_R <- as.numeric(merge_edu_GDP_r$GDP_R)
+merge_edu_GDP_r$Index <- as.numeric(merge_edu_GDP_r$Index)
+
+# plot for Russia's relationship between education index and GDP growth
+p7 <- ggplot(merge_edu_GDP_r)+
+  geom_point(aes(x = merge_edu_GDP_r$Index, y = merge_edu_GDP_r$GDP_R))+
+  geom_smooth(aes(x = merge_edu_GDP_r$Index, y = merge_edu_GDP_r$GDP_R),method = "lm")+
+  labs(title = "relation between education index and GDP growth in Russia", x = "education index", y = "GDP growth rate", caption = "data from world bank")
+
+#plot for realtionship between FDI and GDP of Russia
+merge_FDI_R <- merge(FDI_R, GDP_R, by  ="Year")
+p8 <- ggplot(merge_FDI_R)+
+  geom_point(aes(x = merge_FDI_R$FDI, y = merge_FDI_R$GDP_R))+
+  geom_smooth(aes(x = merge_FDI_R$FDI, y = merge_FDI_R$GDP_R ),method = "lm")+
+  labs(title = "Relationship between FDI and GDP of Russia", caption = "data from World Bank", x = "FDI", y = "GDP")
+  
+
+
 
